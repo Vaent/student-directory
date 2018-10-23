240e9925a2cd9c80a13ec3ff70fc8ab1af10aa52
Al <>
2018-10-23 11:15:56 +0000

    added option to save details to students.csv file

--- directory.rb
+++ directory.rb
@@ -14,6 +14,8 @@ def get_command
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -24,6 +26,7 @@ end
 def print_menu
   puts "1. Input student details"
   puts "2. Display list of students"
+  puts "3. Save list of students to file (students.csv)"
   puts "9. Exit the program"
 end
 
@@ -36,6 +39,16 @@ def input_students
     puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def show_students
